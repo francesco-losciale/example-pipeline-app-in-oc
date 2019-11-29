504eac83314d2a0006f6e874db3ea19e9bef9150
Francesco Losciale <francesco.losciale@thoughtworks.com>
2019-11-29 21:53:33 +0000

    initial commit

---
+++ src/main/java/com/example/springjavaapp/SpringJavaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjavaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springjavaapp/SpringJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjavaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
